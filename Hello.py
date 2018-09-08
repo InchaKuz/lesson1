cd3aea9211c25d07de033e0dd64e0e676d721421
bladerrunner <bladerrunner@bladerrunners-MacBook-Pro.local>
2018-09-08 16:51:37 +0300

    First commit

---
+++ Hello.py
@@ -0,0 +1,2 @@
+name = 'Инна'
+print('name')
---
+++ deffunc.py
@@ -0,0 +1,13 @@
+def get_summ(one, two, delimeter='&'):
+	a = one.upper()
+	b = two.upper()
+	return str(a) + str(delimeter) + str(b)
+	
+
+c = get_summ('Learn', 'python')
+
+print(c)
+
+
+
+
